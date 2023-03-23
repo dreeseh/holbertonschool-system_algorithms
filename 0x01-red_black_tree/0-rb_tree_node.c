86a97de188288303701d1ed14842bcee0bb7a103
dreeseh <1859@holbertonschool.com>
2023-03-23 15:17:16 +0000

    betty & malloc check

--- 0x01-red_black_tree/0-rb_tree_node.c
+++ 0x01-red_black_tree/0-rb_tree_node.c
@@ -1,10 +1,21 @@
 #include "rb_trees.h"
 
+/**
+ * rb_tree_node - creates a Red-Black Tree node
+ * 
+ * @parent: pointer to the parent of the node to create
+ * @value: the value to put into the new node
+ * @color: the color of the node
+ * Return: the new rb_tree_t* node
+ */
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 {
 	rb_tree_t *new_node;
 
 	new_node = malloc(sizeof(rb_tree_t));
+	if (!new_node)
+		return NULL;
+
 	new_node->n = value;
 	new_node->color = color;
 	new_node->parent = parent;
