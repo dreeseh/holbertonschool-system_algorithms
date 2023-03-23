0bda961a286879f84f8f0118636ecc75123b7256
dreeseh <1859@holbertonschool.com>
2023-03-23 15:11:28 +0000

    files for task 0

---
+++ 0x01-red_black_tree/0-rb_tree_node.c
@@ -0,0 +1,15 @@
+#include "rb_trees.h"
+
+rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
+{
+	rb_tree_t *new_node;
+
+	new_node = malloc(sizeof(rb_tree_t));
+	new_node->n = value;
+	new_node->color = color;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
