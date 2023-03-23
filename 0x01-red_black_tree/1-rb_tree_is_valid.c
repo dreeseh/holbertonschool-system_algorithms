3d07f66770ae693a9bc453eb8897c297741b4eec
dreeseh <1859@holbertonschool.com>
2023-03-23 19:28:30 +0000

    removed c++ style coments

--- 0x01-red_black_tree/1-rb_tree_is_valid.c
+++ 0x01-red_black_tree/1-rb_tree_is_valid.c
@@ -1,6 +1,6 @@
 #include "rb_trees.h"
 
-// Returns the number of black nodes on the path from root to the given node
+/* Returns the number of black nodes on the path from root to the given node */
 int countBlackNodes(const rb_tree_t *node) {
 	int blackCount = 0;
 
@@ -14,28 +14,28 @@ int countBlackNodes(const rb_tree_t *node) {
 	return (blackCount);
 }
 
-// Returns true if the given node is a leaf node
+/* Returns true if the given node is a leaf node */
 int isLeafNode(const rb_tree_t *node)
 {
 	return (node == NULL || (node->left == NULL && node->right == NULL));
 }
 
-// Returns true if the given node and its children satisfy the Red-Black Tree properties
+/* Returns true if the given node and its children satisfy the Red-Black Tree properties */
 int isRedBlackTreeHelper(const rb_tree_t *node, int blackCount, int* numNodes)
 {
 	if (node == NULL)
 		return (0);
 
-	// Check for consecutive red nodes
+	/* Check for consecutive red nodes */
 	if (node->color == 1 && node->parent != NULL && node->parent->color == 1)
 		return (1);
 
-	// Check if the number of black nodes on the path to this node is the same as other paths
+	/* Check if the number of black nodes on the path to this node is the same as other paths */
 	if (isLeafNode(node))
 	{
 		(*numNodes)++;
 		if (blackCount == -1)
-		{ 	// first leaf node encountered
+		{ 	/* first leaf node encountered */
 			blackCount = countBlackNodes(node);
 		}
 		else if (blackCount != countBlackNodes(node))
@@ -43,12 +43,12 @@ int isRedBlackTreeHelper(const rb_tree_t *node, int blackCount, int* numNodes)
 			return (1);
 		}
 	}
-	// Recursively check the left and right subtrees
+	/* Recursively check the left and right subtrees */
 	return isRedBlackTreeHelper(node->left, blackCount, numNodes) &&
 		isRedBlackTreeHelper(node->right, blackCount, numNodes);
 }
 
-// Returns true if the given binary tree is a valid Red-Black Tree
+/* Returns true if the given binary tree is a valid Red-Black Tree */
 int rb_tree_is_valid(const rb_tree_t *root)
 {
 
