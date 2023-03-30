4889450266833ad95fd0c16ccc13c9d410e1ee95
dreeseh <1859@holbertonschool.com>
2023-03-30 01:10:22 +0000

    empty trans unit?

--- 0x02-graphs/graphs.h
+++ 0x02-graphs/graphs.h
@@ -78,6 +78,6 @@ vertex_t *graph_add_vertex(graph_t *graph, const char *str);
 int graph_add_edge(graph_t *graph, const char *src, const char *dest, edge_type_t type);
 void graph_delete(graph_t *graph);
 size_t depth_first_traverse(const graph_t *graph, void (*action)(const vertex_t *, size_t));
-size_t breadth_first_traverse(const graph_t *graph, void (*action)(const vertex_t *v, size_t depth));
+size_t breadth_first_traverse(const graph_t *g, void (*a)(const vertex_t *, size_t));
 
 #endif /* GRAPHS_H */
