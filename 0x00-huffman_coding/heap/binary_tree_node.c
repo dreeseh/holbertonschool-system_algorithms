b64614751ebdadef1e67cc20216b30c6da3aa6ff
dreeseh <1859@holbertonschool.com>
2023-04-04 21:41:16 +0000

    betty fixes

--- 0x00-huffman_coding/heap/binary_tree_node.c
+++ 0x00-huffman_coding/heap/binary_tree_node.c
@@ -5,7 +5,7 @@
  *
  * @parent: pointer to the parent node of the node to be created
  * @data: the data to be stored in the node
- * Return a pointer to the created node or NULL if it fails
+ * Return: a pointer to the created node or NULL if it fails
  */
 binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data)
 {
