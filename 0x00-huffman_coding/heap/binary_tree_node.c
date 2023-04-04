4401b05004557c58c4156f94b33e1d1e4845536d
dreeseh <1859@holbertonschool.com>
2023-04-04 21:40:33 +0000

    betty fixes

--- 0x00-huffman_coding/heap/binary_tree_node.c
+++ 0x00-huffman_coding/heap/binary_tree_node.c
@@ -1,5 +1,12 @@
 #include "heap.h"
 
+/**
+ * binary_tree_node - creates a generic Binary Tree node
+ *
+ * @parent: pointer to the parent node of the node to be created
+ * @data: the data to be stored in the node
+ * Return a pointer to the created node or NULL if it fails
+ */
 binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data)
 {
 	binary_tree_node_t *new_node;
