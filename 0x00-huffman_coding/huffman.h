46eacbcf30a2ab558784b317db161f52e9031bf5
dreeseh <1859@holbertonschool.com>
2023-04-24 17:07:09 +0000

    files for task *

--- 0x00-huffman_coding/huffman.h
+++ 0x00-huffman_coding/huffman.h
@@ -23,5 +23,6 @@ typedef struct symbol_s
 /* PROTOTYPES */
 symbol_t *symbol_create(char data, size_t freq);
 heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
+int data_cmp_huffman(void *data1, void *data2);
 
 #endif /* HUFFMAN_H */
