555285928ee35ca28d81d8b9550468cfebc17864
dreeseh <1859@holbertonschool.com>
2023-04-24 16:26:25 +0000

    fies for task 2

--- 0x01-pathfinding/pathfinding.h
+++ 0x01-pathfinding/pathfinding.h
@@ -21,6 +21,36 @@ typedef struct point_s
 	int y;
 } point_t;
 
+/**
+ * struct a_start_s - Structure node representing a vertece
+ *
+ * @index: Index of the vertex in the adjency list.
+ *   When a new vertex is added, this index becomes the number of vertices
+ *   in the adjency list before it is added.
+ * @content: Custom data stored in the vertex (here, a string)
+ * @x: Vertex X coordinate
+ * @y: Vertex Y coordinate
+ * @edges: Pointer to the head node of the linked list of edges
+ * @parent: parent node
+ * @distance: min distance from node to node
+ * @heuristic: euclidian distance from current node to target node
+ * @visited: check if the node has been visited
+ * @cost_path: the addition between distace and heuristic
+ */
+typedef struct a_start_s
+{
+	size_t		index;
+	char		*content;
+	int		x;
+	int		y;
+	edge_t		*edges;
+	int		parent;
+	int		distance;
+	int		heuristic;
+	char		visited;
+	int		cost_path;
+} a_start_t;
+
 /* PROTOTYPES */
 queue_t *backtracking_array(char **map, int rows, int cols,
 			point_t const *start, point_t const *target);
